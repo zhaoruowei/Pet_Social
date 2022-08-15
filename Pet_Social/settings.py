18db1f35cdc4d076ce771aa48cacd7ee07dbd9ca
zhaoruowei <38174230+zhaoruowei@users.noreply.github.com>
2022-08-15 05:46:58 +0100

    Update settings.py

--- Pet_Social/settings.py
+++ Pet_Social/settings.py
@@ -155,9 +155,7 @@ CACHES = {
     }
 }
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_ROOT = os.path.join("/tmp/", "media")
-# MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 MEDIA_URL = "/media/"
 
 INTERNAL_IPS = ['127.0.0.1']
